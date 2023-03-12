649d9b0743bf8b2a42854900706ee6e758f472c0
michalnamelast <mjaronczyk1993@gmail.com>
2023-03-12 22:51:34 +0000

    better

--- lotto.py
+++ lotto.py
@@ -2,43 +2,76 @@ import random
 
 class Lotto:
     def __init__(self):
-        self.moje_liczby = [5,7,14,23,34,39]
+        self.chosen_numbers = [5,7,14,23,34,39]
 
-    def los(self):
-        self.losy = [random.randint(1, 49) for element in range(6)]
-        self.trafione = [element for element in self.moje_liczby if element in self.losy]
-        return self.trafione
+    def bet(self):
+        self.drawn_numbers = self._generate_number()
+        self.hits = self._check_matches()
+        return self.hits
+
+    def _generate_number(self):
+        return [random.randint(1, 49) for element in range(6)]
+
+    def _check_matches(self):
+        return [element for element in self.chosen_numbers if element in self.drawn_numbers]
 
 
-class Licznik:
+class Counter:
     def __init__(self):
-        self.budzet = 0
-        self.wygrana = 0
-        self.trafienia = {0:0, 1:0, 2:0, 3:24, 4:204.9, 5:5997.5, 6:4059640}
+        self.budget = 0
+        self.summary_win = 0
+        self.hit_price = {0:0, 1:0, 2:0, 3:24, 4:204.9, 5:5997.5, 6:4059640}
 
-    def licz(self, punkty):
-        self.budzet += -3
-        self.wygrana += self.trafienia[punkty]
-        self.wygrana = round(self.wygrana, 2)
-        return f'    Wygrano Łącznie: {self.wygrana}zł     Przeznaczono na Zakłady: {self.budzet}zł'
+    def count(self, points):
+        self._update_budget()
+        self._update_winnings(points)
+        return f' Summary win: {self.summary_win}zł Budget: {self.budget}zł'
+
+    def _update_budget(self):
+        self.budget -= 3
+
+    def _update_winnings(self, points):
+        self.summary_win += self.hit_price[points]
+        self.summary_win = self._round_winnings(self.summary_win)
+
+    def _round_winnings(self, number):
+        return round(number, 2)
 
 
-class Jedziemy:
+
+class LetsRoll:
     def __init__(self):
-        self.ilosc_prob = 0
-        self.gra = Lotto()
-        self.licznik = Licznik()
+        self.trials_number = 0
+        self.game = Lotto()
+        self.counter = Counter()
 
     def start(self):
-        trafy = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
-        while len(self.gra.los()) != 6:
-            trafy[len(self.gra.los())] += 1
-            self.ilosc_prob += 1
-            print(f'próba: {self.ilosc_prob}, {trafy}, {self.licznik.licz(len(self.gra.los()))}')
-        trafy[6] += 1
-        self.ilosc_prob += 1
-        print(f'próba: {self.ilosc_prob}, {trafy}, {self.licznik.licz(6)}')
-        print('MAMY 6!!!!')
+        hits = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+        while hits[6] == 0:
+            game_result = self.game.bet()
+            self.trials_number += 1
+            hits[len(game_result)] += 1
+            Test(self.trials_number, len(game_result))
+            self._display_result(hits)
+        print('We got the 6!')
+
+    def _display_result(self, hits):
+        print(f'attempt n0: {self.trials_number}, {hits}, {self.counter.count(len(self.game.bet()))}')
+        # print(self.counter.count(len(self.game.bet())))
 
 
-Jedziemy().start()
+class Test:
+    def __init__(self, attempt, hits):
+        self.attempt_number = attempt
+        self.hits = hits
+        self.show_win_details()
+
+    def show_win_details(self):
+        if self.hits == 5 or self.hits == 6:
+            with open('trafienia.txt', 'a') as file:
+                text = f'{self.attempt_number}, {self.hits} \n'
+                file.write(text)
+
+
+
+LetsRoll().start()
